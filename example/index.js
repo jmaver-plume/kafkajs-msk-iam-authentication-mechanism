a547903ca90db30553fcd5dd2610a86b043ac7ae
Jaka Maver <jmaver@plume.com>
2021-07-12 16:35:57 +0200

    Add test for authentication payload creator

--- example/index.js
+++ example/index.js
@@ -1,27 +1,27 @@
 const { Kafka, AuthenticationMechanisms } = require('kafkajs')
-const { Mechanism, Type } = require('../src');
-AuthenticationMechanisms[Type] = () => Mechanism;
+const { Mechanism, Type } = require('../src')
+AuthenticationMechanisms[Type] = () => Mechanism
 
 const kafka = new Kafka({
-    brokers: process.env.BROKERS.split(','),
-    clientId: 'consumer',
-    ssl: true,
-    sasl: {
-        mechanism: Type
-    },
+  brokers: process.env.BROKERS.split(','),
+  clientId: 'consumer',
+  ssl: true,
+  sasl: {
+    mechanism: Type
+  }
 })
 
 async function run () {
-    const admin = kafka.admin()
-    await admin.connect()
-    const topics = await admin.listTopics();
-    console.log('Topics: ', topics)
-    await admin.disconnect();
+  const admin = kafka.admin()
+  await admin.connect()
+  const topics = await admin.listTopics()
+  console.log('Topics: ', topics)
+  await admin.disconnect()
 }
 
 run()
   .then(() => process.exit(0))
   .catch((err) => {
-      console.error('Error: ', err)
-      process.exit(1)
+    console.error('Error: ', err)
+    process.exit(1)
   })
